de0dc446ec3b0a5b2fea8a908c0a1fbe649d14f2
Arpit Sardana <arpitnd1997@gmail.com>
2020-06-26 23:01:40 +0530

    Update LinkedListAll.cpp

--- LinkedListAll.cpp
+++ LinkedListAll.cpp
@@ -5,3 +5,46 @@ Linked List DS Preparation
 Maintainer: Arpit Sardana
 
 */
+
+using namespace std;
+#include <bits/stdc++.h>
+#define ll long long 
+
+struct Node{
+    ll data;
+    Node* next;
+};
+
+Node* head;
+
+void printLL(){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }cout<<endl<<endl;
+}
+
+void insertValueAtBeg(ll data){
+    Node* temp = new Node();
+    temp->data = data;
+    temp->next = head;
+    head = temp;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    head = NULL;
+    for(ll i=0;i<5;i++){
+        ll num;
+        cin>>num;
+        insertValueAtBeg(num);
+        printLL();
+    }
+    
+    printLL();
+    
+	return 0;
+}	
